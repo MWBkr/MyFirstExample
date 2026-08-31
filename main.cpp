8517f881e8b23c2a0604786d84357a10e419a8b9
Matthew Baker <mwbakermail@gmail.com>
2026-08-31 11:38:37 -0700

    Fixed: Divide by zero problem, close #61

--- main.cpp
+++ main.cpp
@@ -7,19 +7,26 @@ using std::cout;
 
 int main()
 {
-    cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    cout << "Hi, please enter two whole numbers: ";
+	cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+	cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+	int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+	cin >> x >> y;
+	cout << "Addition: " << x + y << endl;
+	cout << "Subtraction: " << x - y << endl;
+	cout << "Multiplication: " << x * y << endl;
+	if(y != 0)
+	{
+		cout << "Division: " << x / y << endl;
+		cout << "Remainder: " << x % y << endl;
+	}
+	else
+	{
+		cout << "Dividing by zero is not a number." << endl;
+	}
+	cout << "Square Root: " << sqrt(x) << endl;
+	cout << "Square: " << pow(x, y) << endl;
 
-    return 0;
+	return 0;
 }
